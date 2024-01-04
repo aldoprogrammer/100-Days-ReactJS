34f7205aa9715c72eedcb391ab97e08bdaaeacdd
aldoprogrammer <107610584+aldoprogrammer@users.noreply.github.com>
2024-01-04 23:54:49 +0700

    calculator FE

---
+++ calculator-reactjs/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'>12,235 *</div>
+        <div className='current-operand'>12,234</div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>%</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>+</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    </div>
+  );
+}
+
+export default App;
