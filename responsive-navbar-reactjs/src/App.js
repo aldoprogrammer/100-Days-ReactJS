7e28bf325d7af0d0d608a26d77a71d0a9627ec8b
Aldo Lata Soba <107610584+aldoprogrammer@users.noreply.github.com>
2024-01-06 18:06:13 +0700

    Add files via upload

---
+++ responsive-navbar-reactjs/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <Navbar />  
+    <div className='bg-indigo-600 w-full h-screen'>
+    </div>
+    </>
+      
+
+  );
+}
+
+export default App;
