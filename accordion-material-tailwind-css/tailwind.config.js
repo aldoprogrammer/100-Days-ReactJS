75cb1d7d15067445229aef7d6b31b2a77711d61e
Aldo Lata Soba <107610584+aldoprogrammer@users.noreply.github.com>
2024-01-20 08:45:07 +0700

    Add files via upload

---
+++ accordion-material-tailwind-css/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
